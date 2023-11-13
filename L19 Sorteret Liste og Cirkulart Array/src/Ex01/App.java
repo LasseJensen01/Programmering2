b5675bda4383fd149a2be633c9cf5549e86eea3a
Lasse Jensen <eaa23laje@students.eaaa.dk>
2023-11-13 12:05:34 +0100

    L19 Is what it is

--- L19 Sorteret Liste og Cirkulart Array/src/Ex01/App.java
+++ L19 Sorteret Liste og Cirkulart Array/src/Ex01/App.java
@@ -2,10 +2,10 @@ package Ex01;
 
 public class App {
     public static void main(String[] args) {
-        SortedList fuck = new SortedList();
-        fuck.add("B");
-        fuck.add("C");
-        fuck.add("A");
-        fuck.print();
+        SortedList duck = new SortedList();
+        duck.add("B");
+        duck.add("C");
+        duck.add("A");
+        duck.print();
     }
 }
